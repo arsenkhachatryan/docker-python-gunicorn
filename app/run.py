c9d7a5705a36efa95e96abc7643857938ba1f0c7
arsenkhachatryan <arsenkhachatryan@users.noreply.github.com>
2022-06-22 00:04:19 +0200

    rollback

--- app/run.py
+++ app/run.py
@@ -1,10 +1,9 @@
 """Minimal flask application."""
 from flask import Flask, jsonify
-import socket
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return jsonify({"Hello": "World", "HostName": socket.gethostname()})
+    return jsonify({"Hello": "World"})
